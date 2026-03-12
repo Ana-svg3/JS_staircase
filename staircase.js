887473e1357bf900c2c82f784c1a5919fccabdaf
Ana-svg3 <st4843@utr.edu.mx>
2026-03-11 23:31:18 -0600

    Add files via upload

---
+++ staircase.js
@@ -0,0 +1,14 @@
+function staircase(n) {
+  let result = "";
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      result += "#";
+    }
+    result += "\n";
+  }
+
+  console.log(result);
+}
+
+staircase(5);
